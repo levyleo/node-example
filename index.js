256b511bcc64c15238a70519b22eb0f30cfe606d
levy <lqcqc@sina.com>
2016-06-01 13:35:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-const koa = require('koa');
+var restify = require('restify'),
+	async = require('async');
 
-const app = koa();
+function userInfo(names, callback) {
 
-const port = 8008;
+	var client = restify.createJsonClient({
+		url: 'http://192.168.10.18',
+		headers: {}
+	});
+	var name = '';
+	for (var i = names.length - 1; i >= 0; i--) {
+		name = name + 'names[]=' + encodeURIComponent(names[i]) + '&';
+	}
+	if (name != '') {
+		name = '?' + name;
+	}
+	console.log(name);
 
-app.use(function *(){
-  this.body = 'Hello Node!';
+	client.get('/api/users/avatars' + name, function(err, req_, res_, obj) {
+		if (err) {
+			callback(err);
+		} else {
+			callback(null, obj);
+		}
+	});
+}
+
+async.parallel({
+	userInfo1: function(callback) {
+		userInfo(['levy'],callback);
+	},
+	userInfo2: function(callback) {
+		userInfo(['levy'],callback);
+	}
+}, function(err, result) {
+	console.error(err);
+	console.log(result);
 });
-
-app.listen(port, function(){
-  console.log('listening on port', port);
-})

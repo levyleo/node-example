0ae5a411ca6e0a25c57cd04f4ca9eb28ca372f2e
levy <lqcqc@sina.com>
2016-06-01 15:27:11 +0800

    no message

--- index.js
+++ index.js
@@ -1,38 +1,17 @@
-var restify = require('restify'),
-	async = require('async');
+var restify = require('restify');
 
-function userInfo(names, callback) {
 
-	var client = restify.createJsonClient({
-		url: 'http://192.168.10.18',
-		headers: {}
-	});
-	var name = '';
-	for (var i = names.length - 1; i >= 0; i--) {
-		name = name + 'names[]=' + encodeURIComponent(names[i]) + '&';
-	}
-	if (name != '') {
-		name = '?' + name;
-	}
-	console.log(name);
-
-	client.get('/api/users/avatars' + name, function(err, req_, res_, obj) {
-		if (err) {
-			callback(err);
-		} else {
-			callback(null, obj);
-		}
+function user_log(req, res, next) {
+	res.charSet('utf-8');
+	res.contentType = 'json';
+	res.send({
+		's': '1',
+		'us': userEntity
 	});
 }
 
-async.parallel({
-	userInfo1: function(callback) {
-		userInfo(['levy'],callback);
-	},
-	userInfo2: function(callback) {
-		userInfo(['levy'],callback);
-	}
-}, function(err, result) {
-	console.error(err);
-	console.log(result);
+server.get('/log', user_log);
+
+server.listen(8080, function() {
+	console.log('%s listening at %s', server.name, server.url);
 });
